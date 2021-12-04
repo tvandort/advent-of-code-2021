f283f0f5db6ebcb3f3e328c08236a7cc2c039c3f
Tom Van Dort <tom@vandort.dev>
2021-12-04 13:13:55 -0500

    Day 3 part 2.

--- src/lib/day-3/index.test.ts
+++ src/lib/day-3/index.test.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 import { createInputGetter } from '../input';
-import { powerConsumption } from '.';
+import { lifeSupport, powerConsumption } from '.';
 
 const input = createInputGetter(__dirname);
 
@@ -9,5 +9,13 @@ test('example power consumption', async (t) => {
 });
 
 test('actual power consumption', async (t) => {
-  t.is(await powerConsumption(input('./actual.txt')), 198);
+  t.is(await powerConsumption(input('./actual.txt')), 3885894);
+});
+
+test('example life support', async (t) => {
+  t.is(await lifeSupport(input('./example.txt')), 230);
+});
+
+test('actual life support', async (t) => {
+  t.is(await lifeSupport(input('./actual.txt')), 4375225);
 });
--- src/lib/day-3/index.ts
+++ src/lib/day-3/index.ts
@@ -1,8 +1,43 @@
 import { readToArray } from '../readToArray';
 
-export async function powerConsumption(filePath: string) {
-  const data = await readToArray(filePath, (line) => line);
-  const length = data[0].length;
+function toDecimal(binary: string) {
+  return parseInt(binary, 2);
+}
+
+async function getData(filePath: string) {
+  return await readToArray(filePath, (line) => line);
+}
+
+function valueLength(array: string[]) {
+  return array[0].length;
+}
+
+function commonBits(
+  counts: number[],
+  data: string[],
+  mostCommon: string,
+  leastCommon: string
+) {
+  return counts.map((count) => {
+    const halfLength = data.length / 2;
+    if (count >= halfLength) {
+      return mostCommon;
+    }
+
+    return leastCommon;
+  });
+}
+
+function calculateEpsilon(data: string[]) {
+  const counts: number[] = countOnes(data);
+  const epsilonArray = commonBits(counts, data, '0', '1');
+  const epsilon = epsilonArray.join('');
+
+  return epsilon;
+}
+
+function countOnes(data: string[]) {
+  const length = valueLength(data);
 
   const counts: number[] = new Array(length).fill(0);
   for (const reading of data) {
@@ -10,14 +45,56 @@ export async function powerConsumption(filePath: string) {
       counts[index] = counts[index] + parseInt(reading[index]);
     }
   }
+  return counts;
+}
 
-  const gammaArray = counts.map((count) =>
-    count > data.length / 2 ? '1' : '0'
-  );
-  const epsilonArray = gammaArray.map((bit) => (bit === '1' ? '0' : '1'));
+function calculateGamma(data: string[]) {
+  const counts: number[] = countOnes(data);
+  const gammaArray = commonBits(counts, data, '1', '0');
+  const gamma = gammaArray.join('');
 
-  const gamma = parseInt(gammaArray.join(''), 2);
-  const epsilon = parseInt(epsilonArray.join(''), 2);
+  return gamma;
+}
 
-  return gamma * epsilon;
+export async function powerConsumption(filePath: string | string[]) {
+  let data: string[];
+
+  if (typeof filePath === 'string') {
+    data = await getData(filePath);
+  } else {
+    data = filePath;
+  }
+
+  const epsilon = calculateEpsilon(data);
+  const gamma = calculateGamma(data);
+
+  return toDecimal(gamma) * toDecimal(epsilon);
+}
+
+function findLifeSupportValue(
+  data: string[],
+  extractor: (data: string[]) => string
+) {
+  const length = valueLength(data);
+  let candidates = data;
+  for (let index = 0; index < length; index++) {
+    if (candidates.length > 1) {
+      const value = extractor(candidates);
+      candidates = candidates.filter(
+        (candidate) => candidate[index] === value[index]
+      );
+    } else {
+      break;
+    }
+  }
+
+  return candidates[0];
+}
+
+export async function lifeSupport(filePath: string) {
+  const data = await getData(filePath);
+  const oxygen = findLifeSupportValue(data, calculateGamma);
+  const co2 = findLifeSupportValue(data, calculateEpsilon);
+
+  return toDecimal(oxygen) * toDecimal(co2);
 }
