03e6bf67ff114ebe503486ef1b21c352cda2f63e
Tom Van Dort <tom@vandort.dev>
2021-12-05 17:22:42 -0500

    Day 5 part 1.

--- src/lib/day-5/index.test.ts
+++ src/lib/day-5/index.test.ts
@@ -1,11 +1,90 @@
 import test from 'ava';
 import { createInputGetter } from '../input';
-import {} from '.';
+import {
+  countPoints,
+  parseVector,
+  Point,
+  pointsOnVector,
+  pointsThatAppearMoreThanOnce,
+  Vector,
+} from '.';
 
 const input = createInputGetter(__dirname);
 const example = input('example.txt');
 const actual = input('actual.txt');
 
-test('that we can parse the call line', (t) => {
-  t.true(false);
+test('string to vector with points', (t) => {
+  t.deepEqual(parseVector('0,9 -> 5,9'), {
+    from: {
+      x: 0,
+      y: 9,
+    },
+    to: {
+      x: 5,
+      y: 9,
+    },
+  });
+});
+
+test('that a horizontal zero length vector is correct', (t) => {
+  const vector: Vector = { from: { x: 0, y: 0 }, to: { x: 1, y: 0 } };
+  t.deepEqual(pointsOnVector(vector), [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+  ]);
+});
+
+test('that a vertical zero length vector is correct', (t) => {
+  const vector: Vector = { from: { x: 0, y: 0 }, to: { x: 0, y: 1 } };
+  t.deepEqual(pointsOnVector(vector), [
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+  ]);
+});
+
+test('that points can be counted', (t) => {
+  const points: Point[] = [
+    { x: 0, y: 0 },
+    { x: 1, y: 1 },
+    { x: 1, y: 1 },
+  ];
+
+  t.deepEqual(countPoints(points), [
+    {
+      count: 1,
+      key: '(0,0)',
+      point: {
+        x: 0,
+        y: 0,
+      },
+    },
+    {
+      count: 2,
+      key: '(1,1)',
+      point: {
+        x: 1,
+        y: 1,
+      },
+    },
+  ]);
+});
+
+test('example for points > 1', async (t) => {
+  t.is(await pointsThatAppearMoreThanOnce(example), 5);
+});
+
+test('points vector of length > 1', (t) => {
+  const vec: Vector = { from: { x: 0, y: 9 }, to: { x: 5, y: 9 } };
+  t.deepEqual(pointsOnVector(vec), [
+    { x: 0, y: 9 },
+    { x: 1, y: 9 },
+    { x: 2, y: 9 },
+    { x: 3, y: 9 },
+    { x: 4, y: 9 },
+    { x: 5, y: 9 },
+  ]);
+});
+
+test('actual for points > 1', async (t) => {
+  t.is(await pointsThatAppearMoreThanOnce(actual), 3990);
 });
--- src/lib/day-5/index.ts
+++ src/lib/day-5/index.ts
@@ -1,1 +1,101 @@
 import { readToArray } from '../readToArray';
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Vector {
+  from: Point;
+  to: Point;
+}
+
+function parsePoint(pointString: string) {
+  const [x, y] = pointString.split(',');
+  return {
+    x: parseInt(x),
+    y: parseInt(y),
+  };
+}
+
+export function parseVector(vectorString: string) {
+  const [leftString, rightString] = vectorString.split('->');
+  const left = parsePoint(leftString);
+  const right = parsePoint(rightString);
+
+  return {
+    from: left,
+    to: right,
+  };
+}
+
+export function pointsOnVector(vector: Vector): Point[] {
+  const points: Point[] = [];
+
+  if (vector.from.x === vector.to.x && vector.from.y !== vector.to.y) {
+    const minY = Math.min(vector.from.y, vector.to.y);
+    const maxY = Math.max(vector.from.y, vector.to.y);
+    for (let vertical = minY; vertical <= maxY; vertical++) {
+      points.push({ x: vector.from.x, y: vertical });
+    }
+  }
+
+  if (vector.from.x !== vector.to.x && vector.from.y === vector.to.y) {
+    const minX = Math.min(vector.from.x, vector.to.x);
+    const maxX = Math.max(vector.from.x, vector.to.x);
+
+    for (let horizontal = minX; horizontal <= maxX; horizontal++) {
+      points.push({ x: horizontal, y: vector.from.y });
+    }
+  }
+
+  return points;
+}
+
+type PointKey = `(${number},${number})`;
+
+interface PointCounter {
+  key: PointKey;
+  count: number;
+  point: Point;
+}
+
+export function countPoints(points: Point[]) {
+  const pointsMap: { [index: PointKey]: PointCounter } = {};
+  const pointsArray: PointCounter[] = [];
+
+  for (const point of points) {
+    const key: PointKey = `(${point.x},${point.y})`;
+    if (pointsMap[key]) {
+      const counter = pointsMap[key];
+      counter.count += 1;
+    } else {
+      const counter: PointCounter = {
+        key,
+        count: 1,
+        point,
+      };
+      pointsMap[key] = counter;
+      pointsArray.push(counter);
+    }
+  }
+
+  return pointsArray;
+}
+
+export async function pointsThatAppearMoreThanOnce(filePath: string) {
+  const points = (
+    await readToArray(filePath, (line) => pointsOnVector(parseVector(line)))
+  ).flatMap((points) => points);
+
+  const counted = countPoints(points);
+
+  return counted
+    .map((p) => {
+      return p;
+    })
+    .filter((point) => point.count > 1)
+    .map((p) => {
+      return p;
+    }).length;
+}
